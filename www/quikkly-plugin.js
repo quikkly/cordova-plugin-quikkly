3021137c681593b1129e08758a890edbd7808b38
Rob Byrne <rob.byrne@mastercard.com>
2018-02-09 23:00:22 +0000

    basic ios plugin

---
+++ www/quikkly-plugin.js
@@ -0,0 +1,11 @@
+var exec = require('cordova/exec');
+
+function QuikklyPlugin() {
+}
+
+QuikklyPlugin.prototype.openScanner = function (apiKey, success, error) {
+    exec(success, error, 'QuikklyPlugin', 'openScanner', [apiKey]);    
+}
+
+var quikkly = new QuikklyPlugin();
+module.exports = quikkly;
